a43e88e9a725238518c2711b4490ad414ed01620
Dipen Napit <114264588+NapitDipen123@users.noreply.github.com>
2022-11-18 15:11:59 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+
+import Login from "./pages/Login";
+
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+
+        <Route exact path="/ecommerce" element={<Home />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Register" element={<Register />} />
+        <Route exact path="/Login" element={<Login />} />
+        <Route exact path="/Cart" element={<Cart />} />
+        <Route exact path="/Product" element={<Product />} />
+        <Route exact path="/ProductList" element={<ProductList />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
